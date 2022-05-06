b69bc85fd8f7ba43e742a9ef104d34773ab9dc6b
lilith-debug <lilith.nil@mailfence.com>
2022-05-06 16:31:21 -0300

    Create grid

---
+++ js-script.js
@@ -0,0 +1,16 @@
+const container = document.createElement("div");
+const body = document.querySelector("body");
+body.appendChild(container);
+
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    container.appendChild(row);
+    for (let i = 0; i < 16; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell"
+        cell.textContent = "a";
+        row.appendChild(cell);
+    }
+}
